841000218c196b6e582dc8bf3cc9635fdc7cfc3d
Linkontoask <18875142896@163.com>
2018-04-02 13:05:43 +0800

    1.0.0

--- js/main.js
+++ js/main.js
@@ -8,25 +8,34 @@ function app() {
 		this.point = '.'
 		this.top = 0
 		this.obj = []
+		this.jump = null
 	}
 
 	main.prototype = {
 		init: function () {
 			console.log(this.name)
-			this.listenScroll()
 			this.findDocument()
+			if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) { //移动端
+			    console.log('mobile')
+			    this.obj[1].style.display = 'none';
+			    this.obj[2].style.left = '0';
+			    this.obj[2].style.width = '100%';
+			} else {
+				this.listenScroll()
+			}
 		},
 		findDocument: function () {
 			this.obj.push(document.querySelector('#mainPoint'))
 			this.obj.push(document.querySelector('#start'))
+			this.obj.push(document.querySelector('.content'))
+			this.jump = document.querySelectorAll('a')
+			console.log(this.jump)
+			this.onJump()
 			this.top = this.obj[1].getBoundingClientRect().height
 			this.updataDocument()	// 持续更新动画
 		},
 		listenScroll: function () {
 			window.onscroll = (e) => {
-				// console.log(e)
-				// console.log(this.getScrollTop())
-				// console.log(this.getScreen().height)
 				let top = this.getScrollTop()
 				if (top >= this.top) {
 					this.obj[1].style.position = 'fixed';
@@ -60,7 +69,32 @@ function app() {
 		        scrollTop=document.body.scrollTop;   
 		    }   
 		    return scrollTop;   
-		}  
+		},
+		scrollTo: function (id) {
+			console.log($('#'+id))
+			$("html,body").animate({scrollTop: $('#'+id).offset().top}, 300);
+		},
+		// 事件委托
+		onJump: function () {
+			let that = this
+			document.querySelector('.sidebar').onclick = function (e) {
+				if (e.target.localName === 'a') {
+					// console.log(e.target.localName)
+					console.log(e.target.text)
+					that.scrollTo(e.target.text)
+					// console.log(e)
+				}
+			}
+			document.querySelector('.coverMain div a').onclick = function (e) {
+				that.scrollTo('start')
+			}
+			document.querySelector('.mark').onclick = function (e) {
+				if (e.target.localName === 'h2') {
+					console.log(e.target.id)
+					that.scrollTo(e.target.id)
+				}
+			}
+		}
 	}
 
 	var app = new main()
